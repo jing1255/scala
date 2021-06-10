da589a280f24320c62690cdf18699d6e489fbe19
jingwz <jingwz>
2021-06-10 14:20:11 +0800

    first

---
+++ src/main/scala/com/yxt/jing/scalabasic/ScalaBasic.scala
@@ -0,0 +1,32 @@
+package com.yxt.jing.scalabasic
+/**
+ *
+ * scala basic operations
+ *
+ * @author jingwz
+ * @version 1.0
+ * @date 2021-06-10 11:13
+ */
+object ScalaBasic {
+  def main(args: Array[String]): Unit = {
+    println("this is the first scala")
+
+    val salary = 20000
+    val name = "king"
+
+    println(s"name=$name salary=${salary + 5000}")
+
+
+    val sql =
+      """
+        |select name
+        |from
+        |t
+        |where t.id = {}
+        |""".stripMargin
+
+    println(s"sql=$sql")
+
+  }
+
+}
